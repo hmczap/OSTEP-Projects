6db3b4ecf29c5e0a75a5243f90875954a4d2949a
hmczap <114511170+hmczap@users.noreply.github.com>
2023-11-22 02:00:33 -0500

    Update wzip.c
    Included errno for error handling

--- wzip.c
+++ wzip.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 
-int main(){
+int main(int argc, char *argv[]){
+  FILE *fp = NULL;
+  FILE *dest = NULL;
+  errno = 0;
+  char buffer[128];
 
+  fp = fopen(argv[1] "r");
+  dest = fopen(argv[2], "w");
 
-
-  return 0;
+  if (fp == NULL || errno != 0) {
+    printf("wzip: cannot open specified file(s)");
+    exit(1);
+  }
+  while (fgets(buffer, 128, fp); != NULL) {
+    printf("%s", buffer)
+  }
+  fclose(fp);
+  fclose(dest);
+  
+  exit(0);
 }
